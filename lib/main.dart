db9916c4cdbc33531af58296f33c14d2ff47178c
Lukas Nagel <lukas@lna-dev.net>
2023-06-14 06:55:22 +0200

    🎨 Code Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -10,51 +10,39 @@ class FedodoUIHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-
     double width = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(width * 0.01),
-        child: GridView(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(width * 0.01),
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+            ),
+            children: const [
+              AppCard(
+                link: "https://micro.dev.fedodo.social",
+                image: "https://fedodo.org/images/Fedodo%20Circle.png",
+              ),
+              AppCard(
+                link: "https://micro.dev.fedodo.social",
+                image: "https://fedodo.org/images/Fedodo%20Circle.png",
+              ),
+              AppCard(
+                link: "https://micro.dev.fedodo.social",
+                image: "https://fedodo.org/images/Fedodo%20Circle.png",
+              ),
+              AppCard(
+                link: "https://micro.dev.fedodo.social",
+                image: "https://fedodo.org/images/Fedodo%20Circle.png",
+              ),
+              AppCard(
+                link: "https://micro.dev.fedodo.social",
+                image: "https://fedodo.org/images/Fedodo%20Circle.png",
+              ),
+            ],
           ),
-          children: const [
-            AppCard(
-              link: "https://micro.dev.fedodo.social",
-              image: "https://fedodo.org/images/Fedodo%20Circle.png",
-            ),
-            AppCard(
-              link: "https://micro.dev.fedodo.social",
-              image: "https://fedodo.org/images/Fedodo%20Circle.png",
-            ),
-            AppCard(
-              link: "https://micro.dev.fedodo.social",
-              image: "https://fedodo.org/images/Fedodo%20Circle.png",
-            ),
-            AppCard(
-              link: "https://micro.dev.fedodo.social",
-              image: "https://fedodo.org/images/Fedodo%20Circle.png",
-            ),
-            AppCard(
-              link: "https://micro.dev.fedodo.social",
-              image: "https://fedodo.org/images/Fedodo%20Circle.png",
-            ),
-          ],
         ),
       ),
     );
